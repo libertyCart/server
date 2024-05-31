396bab832347abd0fafc8be52898781c5807e8ec
mohit-Byte-Roamer <mohit878421@gmail.com>
2024-05-31 09:19:59 +0530

    first commit

---
+++ src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constant.js";
+
+const ConnectDB = async () => {
+  try {
+    const dbConnection = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `\n MongoDB Connected !! DB HOST : ${dbConnection.connection.host}`
+    );
+  } catch (error) {
+    console.log("DB Error : ", error);
+    process.exit(1);
+  }
+};
+
+export default ConnectDB;
